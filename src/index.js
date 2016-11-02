d9c121c081c81542d8a3862f6273b24944d3b9f8
Martin.Snyder <msnyder@wingspan.com>
2016-11-01 21:32:15 -0400

    Initial implementation and unit test

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import Immutable from 'seamless-immutable';
+
+class PrivateData {
+    constructor(currentData) {
+        this.currentData = Immutable(currentData);
+        this.changeListeners = [];
+    }
+
+    update(path, newValue) {
+        const prevData = this.currentData;
+        const nextData = this.currentData.setIn(path, newValue);
+
+        // Fire notification
+        for (let changeListener of this.changeListeners) {
+            changeListener(prevData, nextData, path);
+        }
+
+        this.currentData = nextData;
+    }
+
+    /*
+     * function changeListener(prevRoot, nextRoot, pathUpdated)
+     */
+    addListener(changeListener) {
+        this.changeListeners.push(changeListener);
+    }
+}
+
+const privateDataMap = new WeakMap();
+
+class Cursor {
+    constructor(privateData, path) {
+        privateDataMap.set(this, privateData);
+        this.path = path;
+
+        Object.freeze(this);
+
+        // We don't want to freeze in this constructor because
+    }
+
+    get data() {
+        let pointer = privateDataMap.get(this).currentData;
+        for (let el of this.path) {
+            pointer = pointer
+                ? pointer[el]
+                : undefined;
+        }
+
+        return pointer;
+    }
+
+    set data(newValue) {
+        privateDataMap.get(this).update(this.path, newValue);
+    }
+
+    refine(subPath) {
+        if (subPath.length === 0) {
+            return this;
+        }
+        else {
+            return new Cursor(privateDataMap.get(this), this.path.concat(subPath));
+        }
+    }
+}
+
+export default class RootCursor extends Cursor {
+    constructor(initialRoot = {}) {
+        super(new PrivateData(initialRoot), []);
+    }
+
+    /*
+     * function changeListener(prevRoot, nextRoot, pathUpdated)
+     */
+    onChange(changeListener) {
+        privateDataMap.get(this).addListener(changeListener);
+    }
+}
---
+++ test/testCursor.js
@@ -0,0 +1,79 @@
+import chai from 'chai';
+import Cursor from '../src/index';
+
+const assert = chai.assert;
+
+describe('Data object with primitive', () => {
+    const cursor = new Cursor('primitive');
+    it('constructs cleanly', () => assert.equal('primitive', cursor.data));
+    it('exposes root immutably', () => chai.expect(() => cursor.data = 'updated').to.throw());
+});
+
+describe('Data object with structure', () => {
+    const cursor = new Cursor({
+        attr: 'structured'
+    });
+    it('constructs cleanly', () => assert.equal('structured', cursor.data.attr));
+    it('exposes root immutably', () => chai.expect(() => cursor.data = { attr: 'updated'}).to.throw());
+    it('exposes attribute immutably', () => chai.expect(() => cursor.data.attr = 'updated').to.throw());
+});
+
+describe('Multiple Data classes', () => {
+    const one = new Cursor('one');
+    const two = new Cursor('two');
+
+    it('Maintain their data independently (WeakMap test)', () => {
+        assert.equal('one', one.data);
+        assert.equal('two', two.data);
+        assert.notEqual(one.data, two.data);
+    });
+});
+
+describe('Cursors', () => {
+    const nested = new Cursor({
+        top: {
+            middle: {
+                bottom: 'nestedValue'
+            }
+        }
+    });
+
+    let changes = [];
+    nested.onChange((prevRoot, nextRoot, pathUpdated) =>
+        changes.push({
+            prevRoot: prevRoot,
+            nextRoot: nextRoot,
+            pathUpdated: pathUpdated
+        })
+    );
+
+    it('Descends structures correctly', () => assert.equal('nestedValue', nested.refine(['top', 'middle', 'bottom']).data));
+    it('Safely returns if the path does not exist', () => assert.equal(undefined, nested.refine(['one', 'two', 'three']).data));
+    it('Can be refined to produce new cursors', () => assert.equal('nestedValue', nested.refine(['top']).refine(['middle', 'bottom']).data));
+    it('Can be used to update the managed data object', () => {
+        const cursor = nested.refine(['top', 'middle', 'bottom']);
+        cursor.data = 'updated';
+
+        assert.equal('updated', cursor.data);
+    });
+    it('Can be used to add to the data object', () => {
+        const cursor = nested.refine(['one', 'two', 'three']);
+        cursor.data = 'added';
+
+        assert.equal('added', cursor.data);
+    });
+    it('Makes added elements immutable as well', () => chai.expect(() => nested.root.one.two.three = 'updated').to.throw());
+    it('Fires change events correctly', () => {
+        assert.equal(2, changes.length);
+
+        // Verify first change
+        assert.equal('nestedValue', changes[0].prevRoot.top.middle.bottom);
+        assert.equal('updated', changes[0].nextRoot.top.middle.bottom);
+        assert.deepEqual(['top', 'middle', 'bottom'], changes[0].pathUpdated);
+
+        // Verify Second change
+        assert.equal(undefined, changes[1].prevRoot.one);
+        assert.equal('added', changes[1].nextRoot.one.two.three);
+        assert.deepEqual(['one', 'two', 'three'], changes[1].pathUpdated);
+    })
+});
